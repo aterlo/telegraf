b0a5029470f656b23ef55b7e858f02c6043a4a23
Dan Siemon <dan@aterlo.com>
2018-02-21 11:05:27 -0500

    Update cumulative converter.

--- plugins/processors/cumulative_converter/cumulative_converter.go
+++ plugins/processors/cumulative_converter/cumulative_converter.go
@@ -1,28 +1,33 @@
 package cumulative_converter
 
 import (
+	"log"
+
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/metric"
 	"github.com/influxdata/telegraf/plugins/processors"
 )
 
 type CumulativeConverter struct {
-	cache map[uint64]*state
+	cache map[uint64]state
 }
 
 func NewCumulativeConverter() telegraf.Processor {
-	cc := &CumulativeConverter{}
-	cc.cache = make(map[uint64]*state)
-	return cc
+	cc := CumulativeConverter{}
+	cc.cache = make(map[uint64]state)
+
+	return &cc
 }
 
 type state struct {
-	fields	map[string]float64
-	name string
-	tags	map[string]string
+	fields map[string]float64
+	name   string
+	tags   map[string]string
 }
 
 var sampleConfig = `
+[[processors.cumulative_converter]]
+        namepass = ["net"]
 `
 
 func (p *CumulativeConverter) SampleConfig() string {
@@ -35,47 +40,57 @@ func (p *CumulativeConverter) Description() string {
 
 func (p *CumulativeConverter) Apply(in ...telegraf.Metric) []telegraf.Metric {
 	out := make([]telegraf.Metric, 0, len(in))
+
 	for _, m := range in {
 		id := m.HashID()
-		s := p.cache[id];
-		if s == nil {
-			// hit a new metric, save its value and discard it
-			s = &state{
+
+		s, found := p.cache[id]
+		if !found {
+			// Hit a new metric, save its value and don't forward it on
+			s = state{
 				name:   m.Name(),
 				tags:   m.Tags(),
 				fields: make(map[string]float64),
 			}
+
 			for k, v := range m.Fields() {
 				if fv, ok := convert(v); ok {
 					s.fields[k] = fv
 				}
 			}
+
 			p.cache[id] = s
+
 			continue
 		}
+
 		metric_reset := false
-		fields := make(map[string]interface{})
+		newFields := make(map[string]interface{})
+
 		for k, v := range m.Fields() {
 			if fv, ok := convert(v); ok {
 				pv := s.fields[k]
 				if fv < pv {
-					// stat reset, discard this
+					// Counter reset, discard this
 					if !metric_reset {
 						metric_reset = true
 					}
 				} else {
-					fields[k] = fv - pv
+					newFields[k] = fv - pv
 				}
+
 				s.fields[k] = fv
 			}
 		}
+
 		if metric_reset {
 			continue
 		}
-		// TODO handle error _
-		newmetric, _ := metric.New(m.Name(), m.Tags(), fields, m.Time())
+
+		newmetric, _ := metric.New(m.Name(), m.Tags(), newFields, m.Time())
 		out = append(out, newmetric)
 	}
+
 	return out
 }
 
@@ -86,6 +101,7 @@ func convert(in interface{}) (float64, bool) {
 	case int64:
 		return float64(v), true
 	default:
+		log.Println("Unhandled metric field type")
 		return 0, false
 	}
 }
